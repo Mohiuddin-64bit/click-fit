6dd81efd0f23cc26440e0fc3e0d85b537fad8857
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-09-11 10:38:30 +0600

    Successfully inserted user data to backend

--- js/main.js
+++ js/main.js
@@ -22,3 +22,38 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $('#registration-form').submit(function(e) {
+      e.preventDefault(); // Prevent the default form submission
+
+      // Get user input from the form
+      const email = $('#email').val();
+      const password = $('#password').val();
+      const type = $('#type').val();
+
+      // Create a JSON object with user data
+      const userData = {
+          email: email,
+          password: password,
+          type: type
+      };
+      // Send a POST request to the registration route
+      $.ajax({
+          method: 'POST',
+          url: 'http://localhost:3000/register', // This should match the registration route in your server.js
+          data: JSON.stringify(userData),
+          contentType: 'application/json',
+          success: function(response) {
+              // Handle the success response (e.g., show a success message)
+              console.log(response);
+              alert('User registered successfully!');
+          },
+          error: function(error) {
+              // Handle any errors (e.g., show an error message)
+              console.error('Registration error:', error);
+              alert('Registration failed. Please try again.');
+          }
+      });
+  });
+});
