4ca3fb79bb40f049c10d1d193c4192bd1c18bc21
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-09-12 09:30:46 +0600

    Error Page added

--- js/main.js
+++ js/main.js
@@ -49,67 +49,67 @@ $(document).ready(function () {
     });
   });
 });
-// $(document).ready(function () {
-//   $("#upload_image_background").on("change", function (e) {
-//     const files = e.target.files;
-//     $("#field").css("background: green")
+$(document).ready(function () {
+  $("#upload_image_background").on("change", function (e) {
+    const files = e.target.files;
+    $("#field").css("background: green")
 
 
-//     if (files && files.length > 0) {
-//       for (let i = 0; i < files.length; i++) {
-//         const file = files[i];
+    if (files && files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
 
-//         const formData = new FormData();
-//         formData.append("image", file);
+        const formData = new FormData();
+        formData.append("image", file);
 
-//         $.ajax({
-//           url: "http://localhost:3000/upload",
-//           type: "POST",
-//           data: formData,
-//           processData: false,
-//           contentType: false,
-//           success: function (response) {
-//             const imageUrl = response.imageUrl;
+        $.ajax({
+          url: "http://localhost:3000/upload",
+          type: "POST",
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function (response) {
+            const imageUrl = response.imageUrl;
 
-//             $(".upload_gallery").append(
-//               `<img src="${imageUrl}" alt="Uploaded Image" class="uploaded-image">`
-//             );
-//           },
-//           error: function (error) {
-//             console.error("Error uploading image:", error);
-//           },
-//         });
-//       }
-//     }
-//   });
-// });
-$(document).ready(function() {
-  // Listen for changes in the file input
-  $('#imageUpload').on('change', function(e) {
-      const file = e.target.files[0];
-      
-      if (file) {
-          // Create a FormData object to send the file to the server
-          const formData = new FormData();
-          formData.append('image', file);
-
-          // Send the image data to the server using AJAX
-          $.ajax({
-              url: '/upload', // Replace with your backend endpoint for image upload
-              type: 'POST',
-              data: formData,
-              processData: false,
-              contentType: false,
-              success: function(response) {
-                  // Handle the server's response, e.g., display a success message or update the UI
-                  $('#previewImage').attr('src', response.imageUrl);
-                  $('#previewImage').show();
-              },
-              error: function(error) {
-                  // Handle errors, e.g., display an error message to the user
-                  console.error('Error uploading image:', error);
-              }
-          });
+            $(".upload_gallery").append(
+              `<img src="${imageUrl}" alt="Uploaded Image" class="uploaded-image">`
+            );
+          },
+          error: function (error) {
+            console.error("Error uploading image:", error);
+          },
+        });
       }
+    }
   });
 });
+// $(document).ready(function() {
+  // Listen for changes in the file input
+//   $('#imageUpload').on('change', function(e) {
+//       const file = e.target.files[0];
+      
+//       if (file) {
+//           // Create a FormData object to send the file to the server
+//           const formData = new FormData();
+//           formData.append('image', file);
+
+//           // Send the image data to the server using AJAX
+//           $.ajax({
+//               url: '/upload', // Replace with your backend endpoint for image upload
+//               type: 'POST',
+//               data: formData,
+//               processData: false,
+//               contentType: false,
+//               success: function(response) {
+//                   // Handle the server's response, e.g., display a success message or update the UI
+//                   $('#previewImage').attr('src', response.imageUrl);
+//                   $('#previewImage').show();
+//               },
+//               error: function(error) {
+//                   // Handle errors, e.g., display an error message to the user
+//                   console.error('Error uploading image:', error);
+//               }
+//           });
+//       }
+//   });
+// });
